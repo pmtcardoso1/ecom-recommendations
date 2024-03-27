86aaf18bb7ed68bd3365bf3afc73794d7d5f3c90
Pedro Cardoso <pmtcardoso@gmail.com>
2024-03-27 18:24:17 +0000

    Add recommendations

--- lib/ecomrecommendations/recommendations/recommendations.ex
+++ lib/ecomrecommendations/recommendations/recommendations.ex
@@ -10,34 +10,75 @@ defmodule Ecomrecommendations.EmbeddingRecommendations do
   @flower_type_weight 0.25
   @composition_weight 0.25
 
-  def get_similar_product(product_id) when is_binary(product_id) do
+  @embeddings_model "distilbert-base-uncased"
+
+  def get_similar_products(product_id) when is_binary(product_id) do
     with [target | _] <- get_product_embeddings(product_id) do
+      total_similarity = get_total_similarity_query(target, [get_max_composition_distance(target)])
+
       (
         from pe in EmbeddedProduct,
-        select: %{
-          external_id: pe.external_id,
-          total_similarity: (
-            fragment("pgml.cosine_similarity(?, ?) * ?", pe.description, ^target.description, ^@description_weight) +
-            fragment("pgml.cosine_similarity(?, ?) * ?", pe.brand_name, ^target.brand_name, ^@brand_name_weight)+
-            fragment("pgml.cosine_similarity(?, ?) * ?", pe.flower_type, ^target.flower_type, ^@flower_type_weight)+
-            fragment("(1 - (? <-> ?) / ?) * ? as total_similarity", pe.composition, ^target.composition, ^get_max_composition_distance(target), ^@composition_weight)
-          )
-        },
+        select: pe.external_id,
         where: pe.external_id != ^product_id,
-        order_by: fragment("total_similarity DESC"),
+        order_by: ^total_similarity,
         limit: 10
       )
       |> Repo.all()
     end
   end
 
-  @spec get_user_recommendations(binary()) :: [%{product_id: <<_::32>>}, ...]
   def get_user_recommendations(user_id) when is_binary(user_id) do
-    [
-      %{product_id: "AAAA"},
-      %{product_id: "BBBB"},
-      %{product_id: "CCCC"},
-    ]
+
+    user_products = get_last_interacted_products(user_id)
+
+    attrs =
+      user_products
+      |> join_product_data()
+      |> generate_embeddings()
+
+    composition_distances = Enum.map(user_products, &get_max_composition_distance(&1))
+    product_ids = Enum.map(user_products, fn product -> product.external_id end)
+
+    total_similarity = get_total_similarity_query(attrs, composition_distances)
+
+    (
+      from pe in EmbeddedProduct,
+      select: pe.external_id,
+      where: pe.external_id not in ^product_ids,
+      order_by: ^total_similarity,
+      limit: 10
+    )
+    |> Repo.all()
+  end
+
+  defp get_total_similarity_query(attrs, composition_distances) do
+    total_similarity =
+      dynamic([pe], 0)
+      |> get_attrs_similarity_query(attrs)
+      |> get_composition_similarity_query(composition_distances)
+
+    dynamic([pe], ^total_similarity + fragment("0 DESC"))
+  end
+
+  defp get_attrs_similarity_query(initial_dynamic, attrs) do
+    [:description, :brand_name, :flower_type]
+    |> Enum.reduce(initial_dynamic, fn field, dynamic ->
+      case field do
+        :description ->
+          dynamic([pe], ^dynamic + fragment("pgml.cosine_similarity(?, ?) * ?", pe.description, ^attrs.description, ^@description_weight))
+        :brand_name ->
+          dynamic([pe], ^dynamic + fragment("pgml.cosine_similarity(?, ?) * ?", pe.brand_name, ^attrs.brand_name, ^@brand_name_weight))
+        :flower_type ->
+          dynamic([pe], ^dynamic + fragment("pgml.cosine_similarity(?, ?) * ?", pe.flower_type, ^attrs.flower_type, ^@flower_type_weight))
+      end
+    end)
+  end
+
+  defp get_composition_similarity_query(initial_dynamic, compositions) do
+    compositions
+    |> Enum.reduce(initial_dynamic, fn {target, max_distance}, dynamic ->
+      dynamic([pe], ^dynamic + fragment("(1 - ((? <-> ?) / ?)) * ?", pe.composition, ^target.composition, ^max_distance, @composition_weight))
+    end)
   end
 
   defp get_product_embeddings(product_id) do
@@ -64,6 +105,45 @@ defmodule Ecomrecommendations.EmbeddingRecommendations do
       )
       |> Repo.one()
 
-    max_distance
+    {target, max_distance}
+  end
+
+  defp get_last_interacted_products(_user_id) do
+    ["BD001", "TW001", "SD001", "GSC001", "WW001"]
+    |> Enum.map(fn external_id -> %{
+      external_id: external_id,
+      description: "Test Description",
+      brand_name: "Test Brand Name",
+      flower_type: "Test Flower Type",
+      composition: [3.14, 2.718, 1.618, 4.669, 0.577, 1.732, 2.302, 1.414, 2.718]
+    } end)
+  end
+
+  defp join_product_data(products) do
+    products
+    |> Enum.reduce(%{
+      description: "",
+      brand_name: "",
+      flower_type: ""
+    }, fn p, acc ->
+      %{
+        description: acc.description <> p.description <> ", ",
+        brand_name: acc.brand_name <> p.brand_name <> ", ",
+        flower_type: acc.flower_type <> p.flower_type <> ", "
+      }
+    end)
+  end
+
+  defp generate_embeddings(target) do
+    (
+      from pe in EmbeddedProduct,
+      select: %{
+        description: fragment("pgml.embed(?, ?)", ^@embeddings_model, ^target.description),
+        brand_name: fragment("pgml.embed(?, ?)", ^@embeddings_model, ^target.brand_name),
+        flower_type: fragment("pgml.embed(?, ?)", ^@embeddings_model, ^target.flower_type),
+      },
+      limit: 1
+    )
+    |> Repo.one()
   end
 end
