245af71ed3564397c05fe13254f1f96429d854d7
Pedro Cardoso <pmtcardoso@gmail.com>
2024-03-28 12:18:44 +0000

    Force build

--- config/dev.exs
+++ config/dev.exs
@@ -11,6 +11,7 @@ config :ecomrecommendations, Ecomrecommendations.Repo,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
+
 # For development, we disable any cache and enable
 # debugging and code reloading.
 #
